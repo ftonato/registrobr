9d262823c9edb4fdc755eeaec46ea4f58604f4df
gpedro <gpedro831@gmail.com>
2016-05-25 17:22:59 -0400

    fix: braziiiiil

--- cli.js
+++ cli.js
@@ -6,15 +6,15 @@ const url = process.argv[2] || '';
 
 function parse(data) {
   if (data.available) {
-    console.log(chalk.green('Domain ' + chalk.bold(data.fqdn) + ' Avaliable'));
+    console.log(chalk.green('Domínio ' + chalk.bold(data.fqdn) + ' está disponível'));
   } else {
-    console.log(chalk.red('Domain not avaliable'));
+    console.log(chalk.red('Domínio ' + chalk.bold(data.fqdn) + ' não está disponível'));
     if (data.reason) {
       console.log(chalk.red.bold(escape.decodeHTML(data.reason)));
     }
 
     if (data.suggestions && data.suggestions.length > 0) {
-      console.log(chalk.yellow('Suggestions: '))
+      console.log(chalk.yellow('Sugestões: '))
       data.suggestions.forEach(function (item) {
         console.log("\t" + chalk.yellow.bold('- ' + data.domain + '.' + item));
       })
@@ -23,17 +23,17 @@ function parse(data) {
 }
 
 if (!url || url.length < 2) {
-  console.log(chalk.red('Please, enter a valid url.'));
+  console.log(chalk.red('Por favor, digite uma url válida.'));
   process.exit(1);
 } else {
-  const spinner = ora({ color: 'yellow', text: (chalk.yellow('Fetching ') + chalk.yellow.bold(url))}).start();
+  const spinner = ora({ color: 'yellow', text: (chalk.yellow('Carregando ') + chalk.yellow.bold(url))}).start();
   rbr(url).then(function (response) {
     spinner.stop();
     parse(response);
     process.exit(0);
   }).catch(function(ex) {
     spinner.stop();
-    console.log(chalk.red('Failed!'));
+    console.log(chalk.red('Alguma coisa de errado não está certo.'));
     console.log(ex);
     process.exit(1);
   });

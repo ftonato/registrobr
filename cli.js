f0ce145be57f4d8bb7ba19f021e241ce299823ff
Gabriel Pedro <gpedro@users.noreply.github.com>
2016-06-21 07:29:15 -0400

    resolves #5 (#6)
    * resolves #5
    
    * test: add more tests
    
    * fix: regex validation
    
    * fix: removing unused variable

--- cli.js
+++ cli.js
@@ -4,7 +4,8 @@ const chalk = require('chalk');
 const ora = require('ora');
 const escape = require('entities');
 const dns = require('dns');
-const url = process.argv[2] || '';
+const url = require('url');
+var domain = process.argv[2] || '';
 
 function parse(data) {
   if (data.available) {
@@ -32,24 +33,54 @@ dns.lookup('www.google.com', err => {
   }
 });
 
-if (!url || url.length < 2) {
+if (!domain) {
   console.log(chalk.red('Por favor, digite uma url válida.'));
   process.exit(1);
-} else {
-  if (url.indexOf('.br') === -1) {
-    console.log(chalk.red('A url informada deve possuir a extensão .br'));
-    process.exit(1);
-  }
-
-  const spinner = ora({ color: 'yellow', text: (chalk.yellow('Carregando ') + chalk.yellow.bold(url))}).start();
-  rbr(url).then(function (response) {
-    spinner.stop();
-    parse(response);
-    process.exit(0);
-  }).catch(function(ex) {
-    spinner.stop();
-    console.log(chalk.red('Alguma coisa de errado não está certo.'));
-    console.log(ex);
-    process.exit(1);
-  });
 }
+
+domain = domain.toLowerCase();
+
+if (domain.indexOf('.br') === -1) {
+  console.log(chalk.red('A url informada deve possuir a extensão .br'));
+  process.exit(1);
+}
+hostname = domain.substr(0, domain.indexOf('.'));
+
+if (hostname.length < 2 || hostname.length > 26) {
+  console.log(chalk.red('O Hostname deve ter no mínimo de 2 e máximo de 26 caracteres.'));
+  process.exit(1);
+}
+
+if (hostname.charAt(0) === '-' || hostname.charAt(hostname.length - 1) === '-') {
+  console.log(chalk.red('O Hostname não deve conter hífen no ínicio ou final.'));
+  process.exit(1);
+}
+
+if (parseInt(hostname) == hostname) {
+  console.log(chalk.red('O Hostname não deve conter apenas números.'));
+  process.exit(1);
+}
+
+var regex = /([a-z0-9àáâãéêíóôõúüç]+)/g;
+var matches;
+var groups = 0;
+while (matches = regex.exec(hostname)) {
+  groups++;
+}
+
+if (groups > 1) {
+  console.log(chalk.red('O Hostname deve ser a-z, 0-9, hífen e os seguintes caracteres acentuados: à, á, â, ã, é, ê, í, ó, ô, õ, ú, ü, ç.'));
+  process.exit(1);
+}
+
+const spinner = ora({ color: 'yellow', text: (chalk.yellow('Carregando ') + chalk.yellow.bold(domain))}).start();
+rbr(domain).then(function (response) {
+  spinner.stop();
+  parse(response);
+  process.exit(0);
+}).catch(function(ex) {
+  spinner.stop();
+  console.log(chalk.red('Alguma coisa de errado não está certo.'));
+  console.log(ex);
+  process.exit(1);
+});
--- test.js
+++ test.js
@@ -20,8 +20,33 @@ test('testando domínio com palavras reservadas', t => {
   });
 });
 
-test('deve retornar uma exceção quando a extensão é inválida', t => {
+test('deve retornar uma exceção quando a extensão não tiver .br', t => {
   t.throws(execa('./cli.js', ['test.com']), /A url informada deve possuir a extensão .br/);
+});
+
+test('deve retornar uma exceção quando a extensão se o tamanho for menor de dois caracteres', t => {
+  t.throws(execa('./cli.js', ['a.com.br']), /O Hostname deve ter no mínimo de 2 e máximo de 26 caracteres./);
+});
+
+test('deve retornar uma exceção quando a extensão se o tamanho for maior que 26 caracteres', t => {
+  const url = 'a'.repeat(27) + '.com.br';
+  t.throws(execa('./cli.js', [url]), /O Hostname deve ter no mínimo de 2 e máximo de 26 caracteres./);
+});
+
+test('deve retornar uma exceção quando a hostname terminar com hífen', t => {
+  t.throws(execa('./cli.js', ['test-.com.br']), /O Hostname não deve conter hífen no ínicio ou final./);
+});
+
+test('deve retornar uma exceção quando o hostname conter apenas números', t => {
+  t.throws(execa('./cli.js', ['32132123.com.br']), /O Hostname não deve conter apenas números./);
+});
+
+test('deve retornar uma exceção quando o hostname conter caracteres inválidos', t => {
+  t.throws(execa('./cli.js', ['te$t.com.br']), /O Hostname deve ser a-z, 0-9, hífen e os seguintes caracteres acentuados: à, á, â, ã, é, ê, í, ó, ô, õ, ú, ü, ç./);
+});
+
+test('deve retornar uma exceção quando a hostname iniciar com hífen', t => {
+  t.throws(execa('./cli.js', ['-test.com.br']), /O Hostname não deve conter hífen no ínicio ou final./);
 });
 
 test('deve retornar uma exceção quando a url é inválida', t => {
